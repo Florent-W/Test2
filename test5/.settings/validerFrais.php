5e69c2c5ac070521e40eb1e304e5ac88b9fd55ec
Florent <florent.weltmann@gmail.com>
2018-02-02 13:11:40 +0100

    Inclusion des mois dans la page de validation

---
+++ test5/.settings/validerFrais.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Gestion des frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Florent WELTMANN <florent.weltmann@gmail.com>
+ * @version   GIT: <0>
+ */
+
+<div id="accueil">
+<h2>
+Gestion des frais<small> - Visiteur :
+<?php
+echo $_SESSION['prenom'] . ' ' . $_SESSION['nom']
+?></small>
+    </h2>
+</div>
+?>
